a5061b698e46e421077f7328b0283e5436080545
JohnnyChuang <johnnychuang@knowhow.fun>
2022-11-25 11:43:59 +0800

    editor

--- server/v1/routes/work/work.go
+++ server/v1/routes/work/work.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"sync"
 
 	middlewares "api_server/internal/middlewares/auth"
 	srv "api_server/internal/services/work"
@@ -33,11 +34,11 @@ func (c *Work) SetRoute(engine *gin.RouterGroup) {
 		g = engine.Group("work")
 	}
 	//g := engine.Group("work")
-	g.GET("pic", c.Action)
+	g.GET("pic", c.Pic)
 	g.GET("print", c.Print)
 }
 
-func (c *Work) Action(ctx *gin.Context) {
+func (c *Work) Pic(ctx *gin.Context) {
 	//const target = "https://tw.yahoo.com"
 	//const target = "https://www.taiwan.net.tw/m1.aspx?sNo=0012076"
 	url := ctx.Query("url")
@@ -73,17 +74,32 @@ func (c *Work) Print(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
+
 	var buffer bytes.Buffer
+	imgContentChannel := make(chan string, len(imgs))
+	var wg, wg2 sync.WaitGroup
+	wg.Add(len(imgs))
+	wg2.Add(1)
+
 	for _, img := range imgs {
-		if len(img) <= 0 {
-			continue
-		}
-		//if idx >= 5 {
-		//	break
-		//}
-		base64Encoding := getDataImageBase64(img)
-		buffer.WriteString(fmt.Sprintf("<img src=\"%s\" />", base64Encoding))
+		go getDataImageBase64Async(img, imgContentChannel, &wg)
 	}
+
+	//主序等channel close會卡死，因此另開一序作寫入buffer
+	go func(channel <-chan string, buffer *bytes.Buffer, wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		for val := range channel {
+			buffer.WriteString(fmt.Sprintf("<img src=\"%s\" />", val))
+		}
+	}(imgContentChannel, &buffer, &wg2)
+
+	//等編碼的序
+	wg.Wait()
+	//等編碼的序完成後，關閉channel( 即不會再有任務寫入至channel中)
+	close(imgContentChannel)
+	//等所有資料都轉移到buffer中。
+	wg2.Wait()
 	ctx.Data(http.StatusOK, "text/html; charset=utf-8", buffer.Bytes())
 }
 
@@ -100,4 +116,20 @@ func getDataImageBase64(source []byte) string {
 	}
 	base64Encoding += base64.StdEncoding.EncodeToString(source)
 	return base64Encoding
+}
+
+func getDataImageBase64Async(source []byte, channel chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var base64Encoding string
+
+	mimeType := http.DetectContentType(source)
+
+	switch mimeType {
+	case "image/jpeg":
+		base64Encoding += "data:image/jpeg;base64,"
+	case "image/png":
+		base64Encoding += "data:image/png;base64,"
+	}
+	base64Encoding += base64.StdEncoding.EncodeToString(source)
+	channel <- base64Encoding
 }
